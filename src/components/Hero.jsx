b1fe0364f23a659239fc3245481d7d21287e889d
saba farzanegan <saba.far.99@gmail.com>
2024-07-20 19:33:45 +0330

    add motion to hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,21 @@ import heroimg from "../assets/images/shoes.png";
 import { FaPlay } from "react-icons/fa";
 import { FaArrowLeft } from "react-icons/fa";
 import { FaArrowRight } from "react-icons/fa6";
-
+import { motion } from "framer-motion";
+const slideUp = (delay) => ({
+  hidden: {
+    opacity: 0,
+    y: 100,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.6,
+      delay: delay,
+    },
+  },
+});
 function Hero() {
   return (
     <section className="bg-primary text-white relative">
@@ -11,17 +25,27 @@ function Hero() {
         {/* brand info */}
         <div className="flex flex-col justify-end py-14 md:py-20">
           <div className="text-center md:text-left space-y-4 lg:max-w-[400px]">
-            <h1 className="text-3xl uppercase font-bold">jorden 1 red</h1>
-            <p>
+            <motion.h1
+              variants={slideUp(0.2)}
+              initial="hidden"
+              animate="show"
+              className="text-3xl uppercase font-bold">
+              jorden 1 red
+            </motion.h1>
+            <motion.p variants={slideUp(0.4)} initial="hidden" animate="show">
               release date <br />
               10/08/2024
               <br />
               color way
               <br />
               red
-            </p>
+            </motion.p>
             {/* select section size */}
-            <div className="max-w-[250px] space-y-3 md:mx-0">
+            <motion.div
+              className="max-w-[250px] space-y-3 md:mx-0"
+              variants={slideUp(0.6)}
+              initial="hidden"
+              animate="show">
               <p>select size (IN)</p>
               <div className="flex flex-wrap gap-3 justify-center md:justify-start relative z-10">
                 <p className="size-box">sm</p>
@@ -35,47 +59,66 @@ function Hero() {
                 <p className="size-box">12</p>
                 <p className="size-box">13</p>
               </div>
-            </div>
+            </motion.div>
           </div>
         </div>
         {/* left side section */}
         <div className="flex flex-col justify-center items-center relative gap-16">
-          <img
+          <motion.img
+            initial={{ opacity: 0, x: 100, rotate: 20 }}
+            whileInView={{ opacity: 1, x: 0, rotate: 0 }}
+            transition={{ duration: 0.6, delay: 0.4 }}
             src={heroimg}
             alt=""
             className="max-w-[400px] md:max-w-[500px] relative z-10 brightness-110"
           />
           {/* play icon */}
-          <div className="flex gap-4 items-center">
+          <motion.div
+            className="flex gap-4 items-center"
+            initial={{ opacity: 0, scale: 0 }}
+            whileInView={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.5, delay: 0.8 }}>
             <button className="bg-red-500 flex items-center justify-center p-3 rounded-full hover:scale-105 duration-200">
               <FaPlay />
             </button>
             <p>PLAY VIDIO</p>
-          </div>
+          </motion.div>
         </div>
         {/* hero image section */}
         {/* right side section */}
         <div className="flex flex-col justify-end items-center lg:px-8 py-16">
-          <p className="text-sm">
+          <motion.p
+            className="text-sm"
+            variants={slideUp(0.8)}
+            initial="hidden"
+            animate="show">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque quo
             quam vero, nihil asperiores harum dolore ut illo earum nisi iusto
             facere fugit ad, animi dolorem, delectus sint repellendus iste.
-          </p>
+          </motion.p>
           {/* icones */}
-          <div className="flex gap-8 pt-6 relative z-20">
+          <motion.div
+            className="flex gap-8 pt-6 relative z-20"
+            variants={slideUp(1)}
+            initial="hidden"
+            animate="show">
             <div className="text-2xl border border-white rounded-full p-2 hover:bg-white hover:text-primary duration-300">
               <FaArrowLeft />
             </div>
             <div className="text-2xl border border-white rounded-full p-2 hover:bg-white hover:text-primary duration-300">
               <FaArrowRight />
             </div>
-          </div>
+          </motion.div>
         </div>
       </div>
       {/* bg text and white */}
-      <p className="text-center text-[240px] md:text-[220px] lg:text-[250px] xl:text-[350px ] font-bold font-aston absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white z-[2]">
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.6, delay: 0 }}
+        className="text-center text-[240px] md:text-[220px] lg:text-[250px] xl:text-[350px ] font-bold font-aston absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-white z-[2]">
         NIKE
-      </p>
+      </motion.p>
       <div className="w-[500px] h-[500px] bg-white/45 blur-3xl rounded-full absolute top-1/2  left-1/2 -translate-x-1/2 -translate-y-1/2"></div>
     </section>
   );
