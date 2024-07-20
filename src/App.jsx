2ccbb861679a4e0f212b63e70a2cbc8632bfecaa
saba farzanegan <saba.far.99@gmail.com>
2024-07-20 18:26:18 +0330

    initialize basic structure

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <main className="overflow-x-hidden">
+      <Navbar />
+      <Hero />
+    </main>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#c11912",
+      },
+      fontFamily: {
+        poppins: ["Poppins,sans-serif"],
+        barlow: "barlow",
+        aston: "aston",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "2rem",
+          lg: "4rem",
+          xl: "5rem",
+          "2xl": "6rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
